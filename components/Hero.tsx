7d9a647d727eddf78f5d831b08eda192dd1d60a5
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2022-10-29 21:28:11 +0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero({ pageInfo}: Props) {
         <BackgroundCircles/>
 <Image className='relative rounded-full mx-auto object-cover' src='/pp.jpg' alt='' width='132px' height='132px' />
         <div className='z-20'>
-            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer/ Designer</h2>
         <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
         <span className='mr-3'>{text}</span>
         <Cursor cursorColor='#F7AB0A'/>
